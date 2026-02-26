99dc0556d72ee813f7331f3bfa5825c4302b2a82
Aroa Cotrufo <aroa.c97@gmail.com>
2026-02-26 11:33:53 +0100

    collegamento completato

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 const endpoint ='https://lanciweb.github.io/demo/api/pictures/'
-const cardEl = document.querySelector('.card')
+const cardContainer = document.querySelector('#card-container')
 
 fetch(endpoint)
 .then(response => response.json())  // Trasforma la risposta in JSON
 .then(data => {            
 
-    console.log(data);
-    console.log(cardEl)
-    cardEl.innerHTML = ""
+   const pictures = data.data || data // Verifica se i dati sono in un oggetto "data" o direttamente nell'array
+    cardContainer.innerHTML = ""          //svuota il contenuto della card prima di aggiungere le nuove immagini
 
-    data.forEach(function(item){
-        const imgEl = document.createElement('img')
-        imgEl.src = item.url
-        imgEl.alt = item.title
-        cardEl.appendChild(imgEl)
+    pictures.forEach(function(item){                        
+        const cardItem = document.createElement('div')
+        cardItem.className = 'card-item'
+
+        cardItem.innerHTML = `
+            <img class="card-pin" src="./img/pin.svg" alt="pallino">
+            <img class="card-img" src="${item.url}" alt="immagine">
+            <p class="date">${item.date}</p>
+            <p class="card-text">${item.title}</p>
+        `
+        cardContainer.appendChild(cardItem)
     })
 })
