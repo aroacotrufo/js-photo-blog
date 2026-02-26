e7951461c545e30ab89a078641f452dcff6fa91b
Aroa Cotrufo <aroa.c97@gmail.com>
2026-02-26 17:47:14 +0100

    Esercizio completato

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
-const endpoint ='https://lanciweb.github.io/demo/api/pictures/'
+const endpoint = 'https://lanciweb.github.io/demo/api/pictures/'
 const cardContainer = document.querySelector('#card-container')
 
 fetch(endpoint)
-.then(response => response.json())  // Trasforma la risposta in JSON
-.then(data => {            
+    .then(response => response.json())  // Trasforma la risposta in JSON
+    .then(data => {
 
-   const pictures = data.data || data // Verifica se i dati sono in un oggetto "data" o direttamente nell'array
-    cardContainer.innerHTML = ""          //svuota il contenuto della card prima di aggiungere le nuove immagini
+        const pictures = data.data || data // Verifica se i dati sono in un oggetto "data" o direttamente nell'array
+        cardContainer.innerHTML = ""          //svuota il contenuto della card prima di aggiungere le nuove immagini
 
-    pictures.forEach(function(item){                        
-        const cardItem = document.createElement('div')
-        cardItem.className = 'card-item'
+        pictures.forEach(function (item) {
+            const cardItem = document.createElement('div')
+            cardItem.className = 'card-item'
 
-        cardItem.innerHTML = `
+            cardItem.innerHTML = `
             <img class="card-pin" src="./img/pin.svg" alt="pallino">
             <img class="card-img" src="${item.url}" alt="immagine">
             <p class="date">${item.date}</p>
             <p class="card-text">${item.title}</p>
         `
-        cardContainer.appendChild(cardItem)
+            cardContainer.appendChild(cardItem)
+            const foto = cardItem.querySelector('.card-img')
+            foto.addEventListener('click', function () {
+                document.querySelector('.overlay').style.display = 'flex'
+                document.querySelector('.overlay-img').src = item.url;
+            })
+        })
     })
-})
-const overlayEl = document.querySelector('.overlay')
-overlayEl.classList.add("visible")
-overlayEl.classList.remove("visible")
+
+
+document.querySelector(".close-btn").onclick = function () {
+    document.querySelector(".overlay").style.display = "none"
+}
+
+
