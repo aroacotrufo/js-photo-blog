7a21a982c5fb52874000abfa8f89d067eefdb00d
Aroa Cotrufo <aroa.c97@gmail.com>
2026-02-26 13:58:29 +0100

    creazione overlay

--- script.js
+++ script.js
@@ -9,15 +9,4 @@ fetch(endpoint)
     cardContainer.innerHTML = ""          //svuota il contenuto della card prima di aggiungere le nuove immagini
 
     pictures.forEach(function(item){                        
-        const cardItem = document.createElement('div')
-        cardItem.className = 'card-item'
-
-        cardItem.innerHTML = `
-            <img class="card-pin" src="./img/pin.svg" alt="pallino">
-            <img class="card-img" src="${item.url}" alt="immagine">
-            <p class="date">${item.date}</p>
-            <p class="card-text">${item.title}</p>
-        `
-        cardContainer.appendChild(cardItem)
-    })
-})
+        const c
